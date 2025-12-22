6676ce6d9c5cb73cd8a4fabf714a7b07d80caeff
Stephan Kulla <git.mail@kulla.me>
2025-12-22 22:34:32 +0100

    wip: Code draft for applying CSS to svg

--- src/create-waving-hand-animation.ts
+++ src/create-waving-hand-animation.ts
@@ -7,10 +7,27 @@ const svgPath = path.join(__dirname, "assets", "waving_hand.svg");
 const svg = fs.readFileSync(svgPath, "utf-8");
 const outputPath = process.argv[2];
 
-renderSVGToPNG(svg, path.join(outputPath, "test.png"));
+const wavingAnimationCSS = `
+  #hand {
+    transform: rotate(30deg);
+    transform-origin: center;
+    transform-box: fill-box;
+  }
+`;
+const newSVG = injectCSSAnimation(svg, wavingAnimationCSS);
+
+renderSVGToPNG(newSVG, path.join(outputPath, "test.png"));
 
 function renderSVGToPNG(svgContent: string, outputFilePath: string) {
   const resvg = new Resvg(svgContent, { fitTo: { mode: "width", value: 512 } });
 
   fs.writeFileSync(outputFilePath, resvg.render().asPng());
+}
+
+function injectCSSAnimation(svgContent: string, css: string): string {
+  const svgWithStyle = svgContent.replace(
+    "</svg>",
+    `<style>${css}</style></svg>`,
+  );
+  return svgWithStyle;
 }
